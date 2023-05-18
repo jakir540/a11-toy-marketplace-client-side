8d12d90516e3a083c2cad1b8e2c87ae5619ae3a7
md jakir hossain <jakirhossainmd540@gmail.com>
2023-05-19 01:11:54 +0600

    react tab implemented and searching option added

--- src/Pages/Home/AllToys/AllToys.jsx
+++ src/Pages/Home/AllToys/AllToys.jsx
@@ -1,31 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import Toy from '../Toy/Toy';
+import React, { useEffect, useState } from "react";
+import Toy from "../Toy/Toy";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const AllToys = () => {
-    const [toys,setToys] = useState([])
+  const [toys, setToys] = useState([]);
+  const [activeTab, setActiveTab] = useState("allToys");
 
-useEffect(()=>{
-    fetch('http://localhost:5000/toys')
-    .then(res => res.json())
-    .then(data => {
+  useEffect(() => {
+    fetch(`http://localhost:5000/searceToyByName/${activeTab}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setToys(data);
+      });
+  }, [activeTab]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/toys")
+      .then((res) => res.json())
+      .then((data) => {
         // console.log(data)
-        setToys(data)
-    })
-},[])
+        setToys(data);
+      });
+  }, []);
 
-    return (
-        <div className='my-10'>
-            <h2 className='text-center font-semibold text-4xl text-orange-500'> All toys {toys.length}</h2>
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
 
+  return (
+    <div className="my-10">
+      <div className="flex  flex-col justify-center items-center gap-3">
+        <button
+          onClick={() => handleTabClick("allToys")}
+          className="btn text-center font-semibold"
+        >
+          {" "}
+          All Category toys {toys.length}
+        </button>
 
-            <div className=' max-w-7xl grid grid-cols-3 mx-auto gap-4 my-16'>
-            {
-                toys.map(toy => <Toy key={toy._id} toy={toy}></Toy>)
-            }
-            </div>
-            
-        </div>
-    );
+        <Tabs>
+          <TabList>
+            <Tab
+              onClick={() => handleTabClick("BusToys")}
+              className="btn text-center font-semibold mx-3 bg-orange-900 border-none "
+            >
+              {" "}
+              Buses
+            </Tab>
+            <Tab
+              onClick={() => handleTabClick("BikeToys")}
+              className="btn text-center font-semibold mx-3 bg-orange-900 border-none "
+            >
+              {" "}
+              Bikes
+            </Tab>
+            <Tab
+              onClick={() => handleTabClick("TruckToys")}
+              className="btn text-center font-semibold mx-3 bg-orange-900 border-none "
+            >
+              {" "}
+              Trucks
+            </Tab>
+          </TabList>
+        </Tabs>
+      </div>
+
+      <div className=" max-w-7xl grid lg:grid-cols-3 mx-auto gap-4 my-16">
+        {toys.map((toy) => (
+          <Toy key={toy._id} toy={toy}></Toy>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllToys;
