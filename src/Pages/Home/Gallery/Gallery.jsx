c36a549e30b5b494a2d2bb90533f35584f9b48aa
md jakir hossain <jakirhossainmd540@gmail.com>
2023-05-19 21:10:07 +0600

    AOS package added

--- src/Pages/Home/Gallery/Gallery.jsx
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Gallery = () => {
+  useEffect(()=>{
+    AOS.init({
+      duration: 3000,
+    });
+  },[])
   return (
     <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
       <div class="-m-1 flex flex-wrap md:-m-2">
 
         
         <div class="flex w-1/2 flex-wrap">
-          <div class="w-1/2 p-1 md:p-2">
+          <div class="w-1/2 p-1 md:p-2" data-aos="fade-up">
             <img
               alt="gallery"
               class="block h-full w-full rounded-lg object-cover object-center"
               src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"
             />
           </div>
-          <div class="w-1/2 p-1 md:p-2">
+          <div class="w-1/2 p-1 md:p-2" data-aos="fade-right">
             <img
               alt="gallery"
               class="block h-full w-full rounded-lg object-cover object-center"
               src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
             />
           </div>
-          <div class="w-full p-1 md:p-2">
+          <div class="w-full p-1 md:p-2" data-aos="fade-up">
             <img
               alt="gallery"
               class="block h-full w-full rounded-lg object-cover object-center"
